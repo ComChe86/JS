e56be7f702da860d4f86afa9059c6ed23521e45a
Andrii Cherkasov <129657553+ComChe86@users.noreply.github.com>
2024-02-10 04:41:43 +0100

    Update js_homework2.js
    my mistake was (userAge !== null)

--- js_homework2.js
+++ js_homework2.js
@@ -1,18 +1,22 @@
 var userAge = prompt("Введіть свій вік");
-if (!isNaN(userAge) && userAge >=0) {
+if (userAge !== null && !isNaN(userAge) && userAge >= 0) {
     var year = userAge % 10;
-    var years = userAge >= 11 && userAge <=20;
+    var years = userAge >= 11 && userAge <= 20;
     var result;
-    if (years) {result = "років";}
-    else if (year ===1) {
-        result = "рік";}
-    else if (year >=2 && year <=4){
-        result = "роки";}
-        else { result = "років"}
-   
 
-alert(`Вам ${userAge} ${result}.`);
-}
-else {
-    alert ("Введіть коректне та не від'ємне число ")
+    if (years) {
+        result = "років";
+    } else if (year === 1) {
+        result = "рік";
+    } else if (year >= 2 && year <= 4) {
+        result = "роки";
+    } else {
+        result = "років";
+    }
+
+    alert(`Вам ${userAge} ${result}.`);
+} else if (userAge !== null) {
+    alert("Введіть коректне та не від'ємне число.");
+} else {
+    alert("Ви скасували введення.");
 }
