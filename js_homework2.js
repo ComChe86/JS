ef302586a7819f742cba3db5d10027e8aba8569a
Andrii Cherkasov <129657553+ComChe86@users.noreply.github.com>
2024-02-11 17:52:53 +0100

    Update js_homework2.js

--- js_homework2.js
+++ js_homework2.js
@@ -15,4 +15,27 @@ alert(`Вам ${userAge} ${result}.`);
 }
 else {
     alert ("Введіть коректне та не від'ємне число ")
+
+}
+
+Задача 2
+
+var mean = "кг";
+var amount = 12;
+
+switch (mean) {
+    case "км":
+        var convertAmount = amount * 1000; 
+        console.log(`${amount} км це ${convertAmount} м`);
+        break;
+    case "год":
+        var convertAmount = amount * 60; 
+        console.log(`${amount} год це ${convertAmount} хв`);
+        break;
+    case "кг":
+        var convertAmount = amount * 1000; 
+        console.log(`${amount} кг це ${convertAmount} г`);
+        break;
+    default:
+        console.log("Невірна одиниця вимірювання");
 }
