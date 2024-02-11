9e8da6c4a3b459a756d9859bb9eb2a9e7ba1baf5
Andrii Cherkasov <129657553+ComChe86@users.noreply.github.com>
2024-02-11 21:20:00 +0100

    Update js_homework3.js

--- js_homework3.js
+++ js_homework3.js
@@ -1,4 +1,4 @@
-Задача 1
+//Задача 1 з використанням "for"
 
 function drawTriangle(height, symbol) {
     for (let i = 1; i <= height; i++) {
@@ -12,18 +12,18 @@ function drawTriangle(height, symbol) {
 
 drawTriangle(5, '*');
 
-Задача 2
+//Задача 1  з використанням "while"
 
-function mean(a, b) {
-    let result = 1;
+function drawTriangle(height, symbol) {
+    let i = 1;
 
-    for (let i = 0; i < b; i++) {
-        result *= a;
+    while (i <= height) {
+        let amount = symbol.repeat(i);
+        console.log(amount);
+        i++;
     }
-
-    return result;
 }
 
+drawTriangle(3, '*');
 
-let result = mean(4, 3);
-console.log(result); 
+
