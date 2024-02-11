626a2f58231668371b8ab2333fdfdb40dd5b0871
Andrii Cherkasov <129657553+ComChe86@users.noreply.github.com>
2024-02-11 17:26:02 +0100

    Update js_homework3.js

--- js_homework3.js
+++ js_homework3.js
@@ -14,16 +14,16 @@ drawTriangle(5, '*');
 
 Задача 2
 
-function mean(a, b) {
+function pow(x, y) {
     let result = 1;
 
-    for (let i = 0; i < b; i++) {
-        result *= a;
+    for (let i = 0; i < y; i++) {
+        result *= x;
     }
 
     return result;
 }
 
 
-let result = mean(4, 3);
+let result = pow(3, 3);
 console.log(result); 
